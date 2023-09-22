3c7060ee85806bc72ad6d53aa1ca2fa94f50830f
Ralph Fang <ralphrainierfang@yahoo.com>
2023-09-21 22:45:33 -0400

    updated for of loop and table list

--- workshop.js
+++ workshop.js
@@ -10,6 +10,20 @@ const froyoFlavors = ["vanilla", "coffee", "strawberry"];
 const flavor = userObjectText.split(",");
 console.log(flavor);
 
+// for of loop to iterate array of flavor
+const flavorCount = {};
+for (const item of flavor) {    // item: flavor is key: pair value
+    if (flavorCount[item]) {    // item is Key, flavor generates value
+        flavorCount[item]++;
+    }
+    else {
+        flavorCount[item] = 1;
+    }
+}
+
+//console.log(flavorCount);
+// user observe table listing of each flavor ordered
+console.table(flavorCount);
 
 // user input
 // vanilla,vanilla,vanilla,strawberry,coffee,coffee
